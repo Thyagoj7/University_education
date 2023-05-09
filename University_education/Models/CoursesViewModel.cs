7412983554f414c9c721512dc92657f1ff6dbf7c
Thyagoj7 <t.y_josef7@hotmail.com>
2023-05-09 16:36:21 -0300

    criada a model para cursos

---
+++ University_education/Models/CoursesViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace University_education.Models
+{
+    public class CoursesViewModel
+    {
+        public string nameCourse { get; set; }
+
+        public string typeCourse { get; set; }
+
+        public string nivelCourse { get; set; }
+
+        public double durationCourse { get; set; }
+
+        public string turnCourse { get; set; }
+
+
+    }
+}
---
+++ University_education/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace University_education.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
